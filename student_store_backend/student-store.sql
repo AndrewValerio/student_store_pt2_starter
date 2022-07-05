918b9f19c85b8d62b5f9d73c081c213e42a759d7
AndrewValerio <andrewvalerio1396@gmail.com>
2022-07-05 11:07:06 -0700

    created tables and created database successfully

--- student_store_backend/student-store.sql
+++ student_store_backend/student-store.sql
@@ -15,3 +15,12 @@ CREATE DATABASE auth_starter_test;
 \connect auth_starter_test
 
 \i auth-starter-schema.sql
+
+\echo 'Delete and recreate student_store db?'
+\prompt 'Return for yes or control-C to cancel > ' answer
+
+DROP DATABASE student_store;
+CREATE DATABASE student_store;
+\connect student_store
+
+\i student-store-schema.sql
